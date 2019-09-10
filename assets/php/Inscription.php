d9b31ff409a1bdd2f68989a6c00f5115a422751d
CharlesBoidin <54268730+CharlesBoidin@users.noreply.github.com>
2019-09-10 14:14:55 +0200

    Mouvement

---
+++ assets/php/Inscription.php
@@ -0,0 +1,9 @@
+<?php
+
+$FirstNameUser = $_POST["LastNameUser"]; $NameUser= $_POST["LastNameUser"];
+$MailUser = $_POST["MailUser"]; $PassWordUser=$_POST["PassWordUser"];
+$AdressUser=$_POST["AdressUser"]; $CpUser=$_POST["CpUser"]; $CityUser=$_POST["CityUser"];
+
+echo $AdressUser." ".$CpUser." ".$CityUser;
+
+?>
