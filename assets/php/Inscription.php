fcc530585a73d4cc8a129badcf7aa37e7d8cb801
CharlesBoidin <54268730+CharlesBoidin@users.noreply.github.com>
2019-09-11 08:34:07 +0200

    update

--- assets/php/Inscription.php
+++ assets/php/Inscription.php
@@ -1,9 +1,20 @@
 <?php
 
-$FirstNameUser = $_POST["LastNameUser"]; $NameUser= $_POST["LastNameUser"];
-$MailUser = $_POST["MailUser"]; $PassWordUser=$_POST["PassWordUser"];
-$AdressUser=$_POST["AdressUser"]; $CpUser=$_POST["CpUser"]; $CityUser=$_POST["CityUser"];
+if(isset($_POST["LastNameUser"])
+{
+    $FirstNameUser = htmlspecialchars ($_POST["LastNameUser"],ENT_QUOTES);
+}
+else{
+    $FirstNameUser =  null;
+}
 
-echo $AdressUser." ".$CpUser." ".$CityUser;
+$FirstNameUser = htmlspecialchars ($_POST["LastNameUser"],ENT_QUOTES); $NameUser= htmlspecialchars ($_POST["LastNameUser"],ENT_QUOTES);
+$MailUser = htmlspecialchars ($_POST["MailUser"],ENT_QUOTES); $PassWordUser= htmlspecialchars ($_POST["PassWordUser"],ENT_QUOTES);
+$AdressUser= htmlspecialchars ($_POST["AdressUser"],ENT_QUOTES); $CpUser=htmlspecialchars ($_POST["CpUser"],ENT_QUOTES); $CityUser= htmlspecialchars ($_POST["CityUser"],ENT_QUOTES);
+$PhoneUser= htmlspecialchars ($_POST["PhoneUser"],ENT_QUOTES); $Siret=htmlspecialchars ($_POST["Siret"],ENT_QUOTES);
+$UserCB= htmlspecialchars ($_POST["UserCB"],ENT_QUOTES); $NumberCB=htmlspecialchars ($_POST["NumberCB"],ENT_QUOTES); $CryptoCB= htmlspecialchars ($_POST["CryptoCB"],ENT_QUOTES);
+$ValidityCB= htmlspecialchars ($_POST["YearCB"],ENT_QUOTES)."/".htmlspecialchars ($_POST["MonthCB"],ENT_QUOTES);
+
+echo $ValidityCB;
 
 ?>
